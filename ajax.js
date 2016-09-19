871f189ca14b84603aba2048de53edeaf61dac5f
Rui Dong <rui.dong.jh@gmail.com>
2016-09-19 16:11:20 -0400

    added button to count, display time, and listing out cards.

--- ajax.js
+++ ajax.js
@@ -9,4 +9,54 @@ $(document).ready(function () {
     });
   });
 
+  $('#step3456').click(function(){
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/pong",
+      method: "GET",
+      data: {},
+      dataType: "text"
+    }).done(function (responseData) {
+        console.log(responseData);
+        $('#step3456').append(responseData)
+      }).fail(function (responseData) {
+        console.log(responseData);
+        $('#step3456').append("sorry this does not work")
+      }).always(function (responseData) {
+        console.log("Hey! request is finished!");
+      })
+  });
+
+  $('#step7').click(function(){
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/count",
+      method: "GET",
+      data: {},
+      dataType: "text"
+    }).done(function (responseData){
+      $('#step7').append(responseData);
+    })
+  });
+
+  $('#step8').click(function(){
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/time",
+      method: "GET",
+      data: {},
+      dataType: "text"
+    }).done(function (responseData){
+      $('#step8').append(responseData);
+    })
+  });
+
+  $('#step9').click(function(){
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/a_car",
+      method: "GET",
+      data: {},
+      dataType: "html"
+    }).done(function (responseData){
+      $('#step9').append(responseData);
+    })
+  });
+
 });
